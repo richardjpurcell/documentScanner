8bc144c620b2ca5052548fad3b717f69f566db62
richardjpurcell <richardjpurcell@yahoo.com>
2020-08-21 13:56:27 -0300

    Added main file

---
+++ submission.cpp
@@ -0,0 +1,11 @@
+/*
+ * File:      submission.cpp
+ * Author:    Richard Purcell
+ * Date:      2020-08-20
+ * Version:   1.0
+ * Purpose:   Align a document
+ * Usage:     $ ./submission
+ * Notes:     Created for OpenCV's Computer Vision 1 Project 3
+ *            
+ *            
+ */
